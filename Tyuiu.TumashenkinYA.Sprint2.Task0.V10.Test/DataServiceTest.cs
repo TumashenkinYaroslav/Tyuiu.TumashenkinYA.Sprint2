d924fec10bffc62ad7a79f2f27c5eb84bc7002e8
eborius321 <eborius321@HONORX16EBORIUS>
2025-10-07 11:12:24 +0500

    Add project files.

---
+++ Tyuiu.TumashenkinYA.Sprint2.Task0.V10.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.TumashenkinYA.Sprint2.Task0.V10.Lib;
+namespace Tyuiu.TumashenkinYA.Sprint2.Task0.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCompareOperations()
+        {
+            DataService ds = new DataService();
+            int x = 1305;
+            int y = 475;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            bool[] wait = new bool[6] {false, true, false, true, false, true};
+
+            CollectionAssert.AreEqual(res, wait);
+        }
+
+    }
+}
---
+++ Tyuiu.TumashenkinYA.Sprint2.Task0.V10/Program.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.TumashenkinYA.Sprint2.Task0.V10.Lib;
+
+Console.Title = "Спринт #2 | Выполнил: Тумашенкин Я. А. | ПКТб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #2                                                               *");
+Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+Console.WriteLine("* Задание #0                                                              *");
+Console.WriteLine("* Вариант #10                                                             *");
+Console.WriteLine("* Выполнил: Тумашенкин Ярослав Анатольевич | ПКТб-25-1                    *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу из операций сравненийи арифметических выражений,     *");
+Console.WriteLine("* которая вернет логическую последовательность:                           *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine(" x = 1305, y = 475");
+
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+DataService ds = new DataService();
+var result = ds.GetCompareOperations(1305, 475);
+Console.WriteLine(String.Join(",", result));
+Console.ReadKey();
