b988a49e330180eaf9f1a020441a0b3802ea5bcf
eborius321 <eborius321@gmail.com>
2025-10-07 13:50:18 +0500

    Task5

---
+++ Tyuiu.TumashenkinYA.Sprint2.Task5.V6.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.TumashenkinYA.Sprint2.Task5.V6.Lib;
+namespace Tyuiu.TumashenkinYA.Sprint2.Task5.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestFindCardNameAndValue()
+        {
+            DataService ds = new DataService();
+            int value1 = 1;
+            int value2 = 6;
+            string res = ds.FindCardNameAndValue(value1, value2);
+            string wait = "шестёрка пик";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.TumashenkinYA.Sprint2.Task5.V6/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.TumashenkinYA.Sprint2.Task5.V6.Lib;
+
+Console.Title = "Спринт #2 | Выполнил: Тумашенкин Я. А. | ПКТб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #2                                                               *");
+Console.WriteLine("* Оператор switch                                                         *");
+Console.WriteLine("* Задание #5                                                              *");
+Console.WriteLine("* Вариант #6                                                              *");
+Console.WriteLine("* Выполнил: Тумашенкин Ярослав Анатольевич | ПКТб-25-1                    *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("*Написать программу, которая использует оператор switch вычисляет требуемо*");
+Console.WriteLine("*ое значение и возвращает результат.                                      *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+DataService ds = new DataService();
+
+Console.WriteLine("Введите номер масти: ");
+int value1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите номер достоинства карты: ");
+int value2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+var result = ds.FindCardNameAndValue(value1, value2);
+Console.WriteLine(result);
+Console.ReadKey();
